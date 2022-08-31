2ec0e2ed05ea7d685b216609a2c773635de312ff
Gabriel Shoyombo <gabrielshoyombo2002@gmail.com>
2022-08-31 23:25:40 +0100

    Added a page for adding new topics, another page for entering topic entries, and one for edit entries

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
 """
 from argparse import Namespace
 from django.contrib import admin
-from django.urls import path, re_path
-from learning_logs.views import homepage, about, topics, topic
+from django.urls import path
+from learning_logs.views import homepage, about, topics, topic, new_topic, new_entry, edit_entry
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage, name="learning_logs"),
     path('about/', about, name="about_page"),
     path('topics/', topics, name="topics"),
-    re_path(r'^topic/(?P<topic_id>\d+)/$', topic, name="topic")
+    path('topic/<int:topic_id>/', topic, name="topic"),
+    path('new_topic/', new_topic, name="new_topic"),
+    path('new_entry/<int:topic_id>/', new_entry, name="new_entry"),
+    path('edit_entry/<int:entry_id>/', edit_entry, name="edit_entry")
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,8 @@
+from django.urls import reverse
 from django.shortcuts import render
-#from django.http import HttpResponse
-from .models import Topic
+from django.http import HttpResponseRedirect
+from .models import Entry, Topic
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -24,3 +26,46 @@ def topic(request, topic_id, *args, **kwargs):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, "topic.html", context)
+
+def new_topic(request, *args, **kwargs):
+    """Page for adding new topics"""
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+    context = {'form': form}
+    return render(request, "new_topic.html", context)
+
+def new_entry(request, topic_id, *args, **kwargs):
+    """Page for writing new topics entries"""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid:
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request, "new_entry.html", context)
+
+def edit_entry(request, entry_id, *args, **kwargs):
+    """A Page for editing entries"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid:
+            form.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic.id]))
+    context = {'form': form, 'entry': entry, 'topic':topic}
+    return render(request, "edit_entry.html", context)
+        
